15f4979ae459645e067d5a4847edbda015c293e2
Daniel Bader <mail@dbader.org>
2016-02-21 11:51:03 +0100

    Add apidoc example

--- web.py
+++ web.py
@@ -6,6 +6,13 @@ import bottle
 
 @bottle.get('/api/v1/random/')
 def get_random_number():
+    """
+    @api {GET} /v1/random/ Generate a random number
+    @apiName GetRandomNumber
+    @apiGroup Random
+
+    @apiDescription desc
+    """
     return {
         'request_id': str(uuid.uuid4()),
         'result': random.random(),
